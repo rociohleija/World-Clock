d0ed8720662f587450cb5f95a044c4fea95a405b
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-08-03 18:53:39 -0500

    Edit to change ID and edit on JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,17 @@
 function updateTime(){
-let houstonElement= document.querySelector("#houston");
-let houstonDateElement= houstonElement.querySelector(".date");
-let houstonTimeElement= houstonElement.querySelector(".time");
-let houstonTime = moment().tz("US/Central");
+ let houstonElement= document.querySelector("#houston");
+ let houstonDateElement= houstonElement.querySelector(".date");
+ let houstonTimeElement= houstonElement.querySelector(".time");
+ let houstonTime = moment().tz("US/Central");
 
 houstonDateElement.innerHTML = houstonTime.format("MMMM Do YYYY");
 houstonTimeElement.innerHTML = houston.format(
     "h:mm:ss [<small>]A[</small>]"
   ); 
-let kualaLumpuElement= document.querySelector("#kuala-lumpur");
-let kualaLumpurDateElement= kualaLumpurElement.querySelector(".date");
-let KualaLumpurTimeElement= kualaLumpurElement.querySelector(".time");
-let KualaLumpurTime = moment().tz("America/Los_Angeles");
+  let kualaLumpurElement= document.querySelector("#kuala-lumpur");
+  let kualaLumpurDateElement= kualaLumpurElement.querySelector(".date");
+  let kualaLumpurTimeElement= kualaLumpurElement.querySelector(".time");
+  let kualaLumpurTime = moment().tz("Asia/Kuala_Lumpur");
 
 kualaLumpurDateElement.innerHTML = kualaLumpur.format("MMMM Do YYYY");
 kualaLumpurTimeElement.innerHTML = kualaLumpurTime.format(
@@ -20,4 +20,3 @@ kualaLumpurTimeElement.innerHTML = kualaLumpurTime.format(
 }
 updateTime();
 setInterval(updateTime, 1000);
-
