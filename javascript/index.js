a5bd146ba393e14bc38410dfcd0d39423826c4de
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-08-03 15:05:14 -0500

    Add on date and time function to my project JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
-alert ("hello world!");
+function updateTime(){
+let houstonElement= document.querySelector("#houston");
+let houstonDateElement= houstonElement.querySelector(".date");
+let houstonTimeElement= houstonElement.querySelector(".time");
+let houstonTime = moment().tz("US/Central");
+
+houstonDateElement.innerHTML = houstonTime.format("MMMM Do YYYY");
+houstonTimeElement.innerHTML = houston.format(
+    "h:mm:ss [<small>]A[</small>]"
+  ); 
+let kualaLumpuElement= document.querySelector("#kuala-lumpur");
+let kualaLumpurDateElement= kualaLumpurElement.querySelector(".date");
+let KualaLumpurTimeElement= kualaLumpurElement.querySelector(".time");
+let KualaLumpurTime = moment().tz("America/Los_Angeles");
+
+kualaLumpurDateElement.innerHTML = kualaLumpur.format("MMMM Do YYYY");
+kualaLumpurTimeElement.innerHTML = kualaLumpurTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+ );
+}
+updateTime();
+setInterval(updateTime, 1000);
+
